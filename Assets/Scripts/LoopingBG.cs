1aaa1c2d4fc0f8d221e1034518841b03bd733042
Steve <steventrouble@gmail.com>
2018-06-16 16:38:19 -0700

    Add LoopingBG

---
+++ Assets/Scripts/LoopingBG.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopingBG : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 renderSize = GetComponent<Collider>().bounds.size;
+        transform.Translate (- Time.deltaTime * Variables.TerrainSpeedPerSecond / 2, 0, 0);
+		if (transform.position.x < - renderSize.x / 4)
+        {
+            transform.Translate(renderSize.x / 2, 0, 0);
+        }
+	}
+}

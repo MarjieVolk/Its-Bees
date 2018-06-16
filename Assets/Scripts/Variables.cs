e64c9b2cf489f0566141f92975afa7e118699add
Marjie <marjie.volk@gmail.com>
2018-06-16 15:54:22 -0700

    GLOBAL STATE

---
+++ Assets/Scripts/Variables.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Variables
+{
+    private static float terrainSpeedPerSecond = 1;
+    public static float TerrainSpeedPerSecond
+    {
+        get { return terrainSpeedPerSecond; }
+        set { terrainSpeedPerSecond = value; }
+    }
+}

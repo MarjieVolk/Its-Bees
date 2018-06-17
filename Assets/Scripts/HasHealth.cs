699ce5e218aa20a2c02202493344d8bca39fb209
Steve <steventrouble@gmail.com>
2018-06-16 17:50:09 -0700

    Add HasHealth component

---
+++ Assets/Scripts/HasHealth.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HasHealth : MonoBehaviour {
+
+    float health = 200;
+
+    void TakeDamage(float damage)
+    {
+        health -= damage;
+    }
+
+    private void Update()
+    {
+        if (health <= 0)
+        {
+            Destroy(this);
+        }
+    }
+}

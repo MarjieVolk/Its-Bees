9aad656cefca7ff928288240ccb045232ff2e9d2
Steve <steventrouble@gmail.com>
2018-06-17 17:50:22 -0700

    Show health bar above enemy after it takes damage

--- Assets/Scripts/MiniHealthBar.cs
+++ Assets/Scripts/MiniHealthBar.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHealthBar : MonoBehaviour {
+public class MiniHealthBar : MonoBehaviour
+{
 
     [SerializeField] public HasHealth player;
     private float startWidth;
@@ -12,19 +13,29 @@ public class PlayerHealthBar : MonoBehaviour {
     SpriteRenderer renderer;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         healthBarHolder = transform.GetChild(0);
         healthText = transform.GetChild(1).gameObject.GetComponent<TextMesh>();
         startWidth = healthBarHolder.localScale.x;
         renderer = healthBarHolder.GetChild(0).GetComponent<SpriteRenderer>();
         renderer.color = Color.green;
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    }
+
+    // Update is called once per frame
+    void Update()
     {
-        healthBarHolder.localScale = 
+        healthBarHolder.localScale =
             new Vector3(player.health / player.maxHealth * startWidth, healthBarHolder.localScale.y);
         healthText.text = player.health + " / " + player.maxHealth;
-	}
+        if (player.health == player.maxHealth)
+        {
+            healthBarHolder.gameObject.SetActive(false);
+            healthText.gameObject.SetActive(false);
+        } else
+        {
+            healthBarHolder.gameObject.SetActive(true);
+            healthText.gameObject.SetActive(true);
+        }
+    }
 }
